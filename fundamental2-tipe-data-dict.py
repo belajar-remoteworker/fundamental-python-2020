811125976ccfd2c74f057ab07311e8b541e83ba9
adinurrahman <adinurrahman01@gmail.com>
2020-08-16 16:58:06 +0700

    belajar fundamental python

---
+++ fundamental2-tipe-data-dict.py
@@ -0,0 +1,20 @@
+"""
+Tipe data dictionary sekedar menghubungkan antara KEY dan VALUE
+KVP = Key Value Pair
+perbedaannya jika menggunakan dict, yang dipakai kurung kurawal {}
+kalo mennggunakan list ( lihat dipelajaran sebelumnya), dia pake kurung siku []
+"""
+
+kamus_ind_eng = {}
+kamus_ind_eng ['anak'] = 'son'
+kamus_ind_eng ['istri'] = 'wife'
+kamus_ind_eng ['ayah'] = 'father'
+kamus_ind_eng ['ibu'] = 'mother'
+
+print(kamus_ind_eng)
+print(kamus_ind_eng['ayah'])
+print(kamus_ind_eng['ibu'])
+print(kamus_ind_eng['anak'])
+print(kamus_ind_eng['istri'])
+
+
